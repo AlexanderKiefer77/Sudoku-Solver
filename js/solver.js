5a7e7e1b5b323125c3c08470f0e77bbe4ca8ca0d
Alexander Kiefer <Alexander.Kiefer77@protonmail.com>
2026-01-01 16:17:13 +0100

    add function startSolve and stopSolve

--- js/solver.js
+++ js/solver.js
@@ -1,5 +1,9 @@
 let solving = false;
 
+function stopSolve() {
+    solving = false;
+}
+
 export function startSolve() {
     solving = true;
     $(".cell")
@@ -10,4 +14,17 @@ export function startSolve() {
     $("#info").text("Solving...");
     $(".cell, #clearBtn").prop("disabled", true);
     $("#sudoku").addClass("solving");
+    const solution = solveSudoku();
+    if (solution == true) {
+        $("#info").text("The Sudoku is solved");
+    } else if (solution == false) {
+        $("#info").text("The Sudoku cannot be solved");
+    }
+    $(".cell, #clearBtn").prop("disabled", false);
+    $("#sudoku").removeClass("solving");
+    stopSolve();
+}
+
+function solveSudoku() {
+
 }
