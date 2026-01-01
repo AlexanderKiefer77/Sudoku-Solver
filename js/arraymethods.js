412c7c794cca469634a645a2a530c5d9fff14351
Alexander Kiefer <Alexander.Kiefer77@protonmail.com>
2026-01-01 16:30:24 +0100

    add function getUnsolvedPosition

--- js/arraymethods.js
+++ js/arraymethods.js
@@ -6,6 +6,15 @@ export function resetArray() {
     array = new Array(9)          // Erzeugt ein Array mit 9 Zeilen
         .fill(0)                  // Initialisiert jede Zeile
         .map(() => new Array(9)   // Erzeugt für jede Zeile ein neues Array mit 9 Spalten
-        .fill(0));                // Füllt jede Zelle mit dem Wert 0
+            .fill(0));            // Füllt jede Zelle mit dem Wert 0
+}
+
+export function getUnsolvedPosition() {
+    const row = array.findIndex((row) => row.some((x) => x == 0));
+    if (row == -1) {
+        return null;
+    }
+    const col = array[row].findIndex((x) => x == 0);
+    return { row, col };
 }
 
